6e3059702802b5882689963ab62096ba1ccbfaa2
Golnaz <golnaz.saraji@gmail.com>
2024-10-09 16:40:08 +0200

    add service type list for customer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,26 @@
 import { useState } from 'react';
+import React from 'react';
+import { Route, Routes, useRoutes} from 'react-router-dom';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import React from 'react';
+
+import GetServices from './Components/Customer/Service/Services';
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+      <Routes>
+          <Route path="/" element={ <GetServices/> }/>
+        
+      </Routes>
     </>
+    
   );
+  
 }
 
 export default App;
---
+++ client/src/Components/Customer/Service/Services.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Service.scss"
+
+
+function GetServices() {
+
+    return (
+        <>
+            <div className='container'>
+                <h2> Select Your Desired Service Type</h2>
+                <ul>
+                    <li>
+                        Service Type 1
+                    </li>
+                    <li>
+                        Service Type 2
+                    </li>
+                    <li>
+                        Service Type 3
+                    </li>
+                    <li>
+                        Service Type 4
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default GetServices
