39acac937669e952219cbb3a9c30d6d58daea8a5
Giuseppe Arbore <s329535@studenti.polito.it>
2024-10-11 16:34:38 +0200

    add the possibility for the officer to see the next number to call

--- client/src/Components/Counter/OfficeDesk.tsx
+++ client/src/Components/Counter/OfficeDesk.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Alert, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-
 
 function OfficeDesk() {
     const { number, ticket_number } = useParams<{ number: string, ticket_number?: string }>();
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleShow = () => setShowAlert(true);
+    const handleClose = () => setShowAlert(false);
 
     return (
-        <div>
-            <h1>Counter Number: {number}</h1>
-            {ticket_number && <h2>Current Customer: {ticket_number}</h2>}
+        <>
+            <h1 style={{ textAlign: 'center', marginTop: '1rem' }}>You are the counter {number}</h1>
 
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '3rem', justifyContent: 'space-around' }}>
-                <Button variant="primary" >Next Customer</Button>   
-                {/* Button to proceed to the next customer (OnClick we need to call Alberto's function) */}
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                {showAlert ? (
+                    <Alert variant="success" onClose={handleClose}>
+                        <Alert.Heading>Next Customer is 123</Alert.Heading>
+                        <p>
+                            When the customer arrives, please click on the button below to proceed.
+                        </p>
+                        <Button onClick={handleClose} variant="outline-success">
+                            Close
+                        </Button>
+                    </Alert>
+            ) : (
+                <>
+                    {ticket_number && <h2>Current Customer: {ticket_number}</h2>}
+                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '3rem', justifyContent: 'space-around' }}>
+                        <Button variant="primary" onClick={handleShow}>Next Customer</Button>
+                    </div>
+                </>
+            )}
 
             </div>
-        </div>
+        </>
     );
 }
 
