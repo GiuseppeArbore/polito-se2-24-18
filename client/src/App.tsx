9c523bb7604d20a17992ea88a81a391149bfe8bc
Golnaz <golnaz.saraji@gmail.com>
2024-10-13 17:42:35 +0200

    merge with OQ-17

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import './App.css';
 
 import GetServices from './Components/Customer/Service/Services';
 import { Route, Routes, useRoutes} from 'react-router-dom';
 
 import GetCounter from './Components/Counter/Counter';
+import React from 'react';
 
 
 
