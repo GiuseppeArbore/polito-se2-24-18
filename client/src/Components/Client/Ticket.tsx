298fc5916d0043fdf4bfd06e9f256deaa4688d54
Luca Bordino <lb@Lucas-MacBook-Pro.local>
2024-10-16 14:04:52 +0200

    add timer and button to go back to select

--- client/src/Components/Client/Ticket.tsx
+++ client/src/Components/Client/Ticket.tsx
@@ -1,12 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { Alert } from 'react-bootstrap';
+import React, { useEffect, useRef, useState } from 'react';
+import { Alert, Button, Card } from 'react-bootstrap';
+import ProgressBar from "@ramonak/react-progress-bar";
 import { useParams } from 'react-router-dom';
 import QRCode from "qrcode";
-
+import { useNavigate } from 'react-router-dom';
 function GetTicket() {
     const url = window.location.href;
     const [dataUrl, setDataUrl] = useState("");
   
+    const navigator = useNavigate();
+    const timer = useRef(0);
+    const [progress, setProgress] = useState(0);
+    const [progressColor, setProgressColor] = useState("#3b82f6")
+    useEffect(() => {
+        timer.current = setInterval(() => {
+            setProgress((prevProgress) => {
+                const newProgress = prevProgress + 5;
+                if (newProgress >= 100) {
+                 navigator('/');
+                }
+                return newProgress;
+            });
+        }, 1500);
+
+        return () => {
+            clearInterval(timer.current);
+        };
+    }, [progress]);
     const { service_type, ticket_number } = useParams<{ service_type: string, ticket_number: string }>();
     const handleQRCodeGeneration = () => {
         QRCode.toDataURL(url, { width: 300 }, (err, dataUrl) => {
@@ -20,13 +40,17 @@ function GetTicket() {
     }, [dataUrl]);
     return (
         <>
-            <h1 style={{ textAlign: 'center', marginTop: '1rem' }}>You are the number {ticket_number}</h1>
-            <h2 style={{ textAlign: 'center' }}>for S{service_type}</h2>
-            <Alert variant="info" style={{ textAlign: 'center' }}>Please wait for your turn</Alert>
-            <img src={dataUrl} alt="qr code" />
-            <p style={{ textAlign: 'center', marginTop: '1rem' }}>
-                Scan the QR code above to get all  the information about your ticket
-            </p>
+            <Card>
+                <h1 style={{ textAlign: 'center', marginTop: '1rem' }}>You are the number {ticket_number}</h1>
+                <h2 style={{ textAlign: 'center' }}>for S{service_type}</h2>
+                <Alert variant="info" style={{ textAlign: 'center' }}>Please wait for your turn</Alert>
+                <img src={dataUrl} alt="qr code" />
+                <p style={{ textAlign: 'center', marginTop: '1rem' }}>
+                    Scan the QR code above to get all  the information about your ticket
+                </p>
+                <Button variant="danger" className='mt-2' onClick={() => { navigator('/') }}>Select new ticket</Button>
+            </ Card>
+            <ProgressBar className='mt-2' completed={progress} bgColor={progressColor} height={"0.7rem"} isLabelVisible={false} />
         </>
     );
 }
