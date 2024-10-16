0130b6697fbf44c56cdc902a68062a25dbc263a5
Giuseppe Arbore <s329535@studenti.polito.it>
2024-10-16 23:06:00 +0200

    link ticket and qrCode

--- client/src/Components/Customer/Service/Services.tsx
+++ client/src/Components/Customer/Service/Services.tsx
@@ -3,8 +3,10 @@ import API from "../../../API"
 import { useEffect, useState } from "react"
 import { Service } from '../../../Models/service'
 import { Button, Card } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function GetServices() {
+    const navigator = useNavigate();
 
     const [serviceList, setServiceList] = useState<Service[]>([]);
     const [error, setError] = useState<string>("");
@@ -35,7 +37,7 @@ function GetServices() {
 
     useEffect(() => {
         const getTicket = async (service_id: number) => {
-            
+
             try {
                 const tkt = await API.getTicket(service_id);
                 setTicket(tkt);
@@ -62,41 +64,40 @@ function GetServices() {
     return (
         <>
 
-        <div>
-            { error == "" && ticket == "" &&
-                <>
-                    <h1>Select Your Desired Service</h1>
-                    {serviceList.map((service: Service) => 
-                        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '3rem', justifyContent: 'space-around' }} key={service.id}>
-                
-                        <Card key={service.description} style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Button value={service.id} variant="primary" onClick={() => generateTicket(service.id)}>
-                                {service.description}
+            <div>
+                {error == "" && ticket == "" &&
+                    <>
+                        <h1>Select Your Desired Service</h1>
+                        {serviceList.map((service: Service) =>
+                            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '3rem', justifyContent: 'space-around' }} key={service.id}>
+
+                                <Card key={service.description} style={{ width: '18rem' }}>
+                                    <Card.Body>
+                                        <Button value={service.id} variant="primary" onClick={() => generateTicket(service.id)}>
+                                            {service.description}
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+
+                            </div>
+                        )}
+                    </>
+                }
+                {
+                    ticket != "" &&
+                    <>
+                        <h1>Your turn is {ticket}</h1>
+                        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '3rem', justifyContent: 'space-around' }}>
+                            <Button variant="danger" onClick={() => { returnToList() }}>Return</Button>
+
+                            <Button variant="primary" onClick={() => navigator(`/ticket/${ticket.split("-")[0].substr(1)}/${ticket.split('-')[1]}`)} >
+                                Show qr code
                             </Button>
-                        </Card.Body>
-                        </Card>
-                
                         </div>
-                    )}
-                </>
-            }
-            {
-                ticket != "" && 
-                <>
-                    <h1>Your turn is </h1>
-                     <Card key={ticket} style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Button value={ticket} variant="primary" >
-                                {ticket}
-                            </Button>
-                        </Card.Body>
-                    </Card>
-                    <Button style={{ backgroundColor: 'green', color: 'white'}} onClick={() => { returnToList() }}>Return</Button>
-                </>
-            }
-        </div>
-            
+                    </>
+                }
+            </div>
+
         </>
     )
 }
