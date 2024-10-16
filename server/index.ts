cd60f7e65a27a08a2061637429a08eed3b38e424
Luca Bordino <lb@Lucas-MacBook-Pro.local>
2024-10-16 10:44:25 +0200

    add docker config

--- server/index.ts
+++ server/index.ts
@@ -11,9 +11,10 @@ const app: express.Application = express();
 const port: number = 3001;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost', 'http://localhost:5173'],
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 initRoutes(app);
